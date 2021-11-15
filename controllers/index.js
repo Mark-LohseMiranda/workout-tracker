856f0bbc49c9a1286b801f6f56e255ae2fc4304c
Mark L-M <mark.lohsemiranda@gmail.com>
2021-11-15 07:59:52 -0800

    new workout works

--- controllers/index.js
+++ controllers/index.js
@@ -3,17 +3,17 @@ const router = express.Router();
 const db = require("../models/exerciseModel");
 
 router.get("/api/workouts", (req, res) => {
-    db.find({})
+  db.find({})
     .then((db) => {
-        console.log(db)
-        // db.aggregate([
-        //   {$addFields: {
-        //       totalDuration: {$sum: "$exercises[0].duration"}
-        //   }}
-        // ]).exec((err, data)=> {
-        //     if (err) console.log(err);
-        //     console.log(data)
-        // });
+      console.log(db);
+      // db.aggregate([
+      //   {$addFields: {
+      //       totalDuration: {$sum: "$exercises[0].duration"}
+      //   }}
+      // ]).exec((err, data)=> {
+      //     if (err) console.log(err);
+      //     console.log(data)
+      // });
       res.json(db);
     })
     .catch((err) => {
@@ -21,8 +21,17 @@ router.get("/api/workouts", (req, res) => {
     });
 });
 
-router.post("/api/workouts", ({ body }, res) => {
-  db.create(body);
+router.post("/api/workouts", (req, res) => {
+  db.create({
+    day: new Date(new Date().setDate(new Date().getDate())),
+    exercises: [],
+  })
+    .then((created) => {
+      res.json(created);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 router.put("/api/workouts/:id", (req, res) => {
@@ -31,8 +40,15 @@ router.put("/api/workouts/:id", (req, res) => {
     { $push: { exercises: req.body } },
     (error, success) => {
       if (error) {
-        res.json(err);
+        res.json(error);
       } else {
+        db.aggregate([
+          {
+            $addFields: {
+              totalDuration: 0,
+            },
+          },
+        ]);
         res.json(success);
       }
     }
