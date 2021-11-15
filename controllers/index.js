a224812b2b9843f92019b60512a428f199e775f3
Mark L-M <mark.lohsemiranda@gmail.com>
2021-11-15 10:03:46 -0800

    everything but last 7 days works

--- controllers/index.js
+++ controllers/index.js
@@ -2,34 +2,33 @@ const express = require("express");
 const router = express.Router();
 const db = require("../models/exerciseModel");
 
-router
-  .get("/api/workouts", (req, res) => {
-    db.aggregate(
-      [
-        { $match: {} },
-        {
-          $addFields: {
-            totalDuration: {
-              $reduce: {
-                input: "$exercises",
-                initialValue: 0,
-                in: {
-                  $add: ["$$value", "$$this.duration"],
-                },
+router.get("/api/workouts", (req, res) => {
+  db.aggregate(
+    [
+      { $match: {} },
+      {
+        $addFields: {
+          totalDuration: {
+            $reduce: {
+              input: "$exercises",
+              initialValue: 0,
+              in: {
+                $add: ["$$value", "$$this.duration"],
               },
             },
           },
         },
-      ],
-      (err, data) => {
-        if (err) {
-          res.json(err);
-        } else {
-          res.json(data);
-        }
+      },
+    ],
+    (err, data) => {
+      if (err) {
+        res.json(err);
+      } else {
+        res.json(data);
       }
-    );
-  });
+    }
+  );
+});
 
 router.post("/api/workouts", (req, res) => {
   db.create({
@@ -58,10 +57,47 @@ router.put("/api/workouts/:id", (req, res) => {
   );
 });
 
-router.get("/aggregate", (req, res) => {
+router.get("/api/workouts/range", (req, res) => {
+  const d = new Date(new Date().setDate(new Date().getDate() - 7));
   db.aggregate(
     [
-      { $match: {} },
+      { $match: { day: { $lt: d } } },
+      {
+        $addFields: {
+          totalDuration: {
+            $reduce: {
+              input: "$exercises",
+              initialValue: 0,
+              in: {
+                $add: ["$$value", "$$this.duration"],
+              },
+            },
+          },
+        },
+      },
+    ],
+    (err, data) => {
+      if (err) {
+        res.json(err);
+      } else {
+        res.json(data);
+      }
+    }
+  );
+});
+
+router.get("/stats", (req, res) => {
+  res.redirect("/stats.html");
+});
+
+router.get("/aggregate", (req, res) => {
+  const d = new Date(new Date().setDate(new Date().getDate() - 7));
+//   db.find({day:{$gt:d}},(err,data)=>{
+//       res.json(data)
+//   })
+  db.aggregate(
+    [
+      { $match: ({ day: { $gt: d } } )},
       {
         $addFields: {
           totalDuration: {
